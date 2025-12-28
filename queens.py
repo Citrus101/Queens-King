abe10d98c5c66aac11719f4615146d5ad79e2dae
Citrus101 <shumansswain@gmail.com>
2025-12-28 08:23:30 +0000

    Create an NxN dict Check before hand to see if two queens are in the same row or column and skip Bug: the implementation of above introduces repetition of a few combinations. This is because I don't update the board for previous queens after current queen loops to its start.

--- queens.py
+++ queens.py
@@ -1,16 +1,43 @@
 from collections import defaultdict
-import board_check
+# import board_check
+import time
+
+def inc_perm():
+    ...
+
+def check_row(board:list, n:int, row:int) -> bool:
+    sum = 0;
+    for i in range(n):
+        idx = row * n + i;
+        sum += board[idx];
+        # if(board[idx] == 1):
+            # sum += check_surrounding(board, n, row, i);
+    
+    return (sum == 1);
 
 def print_board(board:list, n:int):
-    print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
-    print(board)
-    print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
-    print("===============================================================")
-    for i in range(n):
-        for j in range(n):
-            print(what[i * n + j], end=" ")
-        print()
-    print("===============================================================")
+    filename = "board"
+    # print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    # print(board)
+    # print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    # print("===============================================================")
+    append_list_to_file(filename, board);
+    # for i in range(n):
+    #     for j in range(n):
+    #         print(what[i * n + j], end=" ")
+    #     print()
+    # print("===============================================================")
+
+def append_list_to_file(filename: str, items: list) -> None:
+
+    with open(filename, "a") as f:
+        f.write("===============================================================\n")
+        for i in range(n):
+            for j in range(n):
+                f.write(f"{what[i * n + j]} ")
+            f.write("\n")
+        f.write("===============================================================\n")
+
 
 nice = [
     1, 1, 1, 1, 1, 1, 1, 1, 1,
@@ -23,34 +50,32 @@ nice = [
     8, 8, 9, 9, 9, 9, 1, 1, 1,
     8, 8, 8, 9, 9, 9, 9, 1, 1,
 ]
-n = 9
+n = 3
 
 coords_dict = defaultdict(list)
 # color_coords[2] = 3;
 
-for i in range(81):
-    coords_dict[nice[i]].append(i)
+for i in range(1, n + 1):
+    # coords_dict[nice[i]].append(i)
+    for j in range(n):
+        coords_dict[i].append(n * (i - 1) + j)
 
-n = 4
+
 idx_array = [0] * (n + 1)
 curr = n
-coords_dict = {
-    1 : [0, 1, 2, 3],
-    2 : [4, 5, 6, 7],
-    3 : [8, 9, 10, 11],
-    4 : [12, 13, 14, 15],
-}
-
 kill_switch = False;
 count = 0
-while(1):
-    what = [0] * (n * n)
-    for key in coords_dict:
-        what[coords_dict[key][idx_array[key]]] = 1
+# idx = 0
 
-    print_board(what, n)
+start = time.time()
+nice = 0;
+with open("board", "w") as f:
+    ...
 
-
+def inc_perm():
+    
+    global curr
+    global kill_switch
     idx_array[curr] += 1
 
     while(idx_array[curr] >= len(coords_dict[curr])):
@@ -62,15 +87,49 @@ while(1):
         idx_array[curr] = 0
         curr -= 1
         idx_array[curr] += 1
-        if(idx_array[0] >= len(coords_dict[1])): kill_switch = True
+        if(idx_array[1] >= len(coords_dict[1])): kill_switch = True
         if(curr == 0): break;
 
     curr = n 
+
+while(1):
+    what = [0] * (n * n)
+    row  = [0] * (n + 1)
+    col  = [0] * (n + 1)
+    # s = time.time();
+    for key in coords_dict:
+        idx = coords_dict[key][idx_array[key]]
+
+        r = int(idx / n);
+        c = idx % n;
+
+        while(row[r] or col[c]):
+            print_board(what, n);
+            inc_perm();
+            idx = coords_dict[key][idx_array[key]]
+
+            r = int(idx / n);
+            c = idx % n;
+
+        what[idx] = 1
+        row[r] = 1;
+        col[c] = 1;
+        # what[coords_dict[key][idx_array[key]]] = 1
+        # row[]
+        # if(check_row(what, n, idx)): continue
+    inc_perm();
+
+    # print_board(what, n)
+    # e = time.time();
+    # nice += e - s;
+
    
     count += 1;
     if(kill_switch == True): break;
 
     # print(what)
 
-
+end = time.time()
+print(end - start)
+# print(nice)
 print(count)
